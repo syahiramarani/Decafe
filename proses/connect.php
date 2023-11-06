0ba6096ca1a1a8fe048ee6e98651e7a91db63c1b
syahiramarani <syahiramarani@gmail.com>
2023-11-06 23:52:41 +0700

    decafe

--- proses/connect.php
+++ proses/connect.php
@@ -1,6 +1,6 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "db_decafe");
-if (!$conn) {
-    echo "Gagal koneksi";
-}
+    $conn = mysqli_connect("localhost","u837952942_syahira","kQ7>dGuiC8F","u837952942_syahira");
+    if(!$conn){
+        echo "Gagal koneksi";
+    }
 ?>
