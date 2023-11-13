406d53b46105f0db7e763c9187871d00e0c7ec2c
syahiramarani <syahiramarani@gmail.com>
2023-11-13 09:44:26 +0700

    ubah

--- proses/connect.php
+++ proses/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","decafe");
+    $conn = mysqli_connect("localhost","u837952942_syahira","kQ7>dGuiC8F","u837952942_syahira");
     if(!$conn){
         echo "Gagal koneksi";
     }
